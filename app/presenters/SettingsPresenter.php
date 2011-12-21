0fe1f4185fea80eb80a6ee1f6882ee576422f15f
Duke <stukov0210@yahoo.com>
2011-12-21 21:25:04 +0100

    Created Settings and Admin presenters

---
+++ app/presenters/SettingsPresenter.php
@@ -0,0 +1,82 @@
+<?php
+use Nette\Application\UI,
+	Nette\Security as NS;
+/**
+ * Homepage presenter.
+ *
+ * @author  Duke
+ * @package ToDo list
+ */
+
+/**
+ * Homepage Presenter
+ *
+ * @package ToDo list
+ * @subpackage presenters
+ */
+class SettingsPresenter extends BasePresenter
+{
+  protected function getRole($user)
+	{
+
+		if($user->isInRole('1')){
+			$result = dibi::query('SELECT role FROM `roles` WHERE idroles = %i', 1)->fetch();
+		}elseif($user->isInRole('2')){
+			$result = dibi::query('SELECT role FROM `roles` WHERE idroles = %i', 2)->fetch();
+		}else{
+			$result = dibi::query('SELECT role FROM `roles` WHERE idroles = %i', 0)->fetch();
+		}
+
+		return $result->role;
+
+  }
+
+/**
+ * Function that creates menu that depends on login status
+ *
+ * @param boolean $isLogged user login status
+ */
+  protected function makeMenu($isLogged)
+	{
+    if(!$isLogged){
+    $this->template->menuItems = array(
+            'Domů' => 'Homepage:',
+            'Registrace' => 'Registration:',
+            );
+    }else{
+    $this->template->menuItems = array(
+            'Domů' => 'Homepage:',
+            'Nastavení' => 'Settings:',
+            'Odhlášení' => 'Logout:',
+            );
+			if($this->getUser()->isInRole('2')){
+    		$this->template->menuItems = array(
+            'Domů' => 'Homepage:',
+            'Nastavení' => 'Settings:',
+						'Administrace' => 'Admin:',
+            'Odhlášení' => 'Logout:',
+            );
+			}
+    }
+  }
+
+/**
+ * Function puts variables into template
+ */
+  public function renderDefault()
+	{
+	  $this->template->description =  "Semestrální práce pro WA1.";
+	  $this->template->keywords =  "kruzimic, fel, čvut, java, programováni, php, html, css, js, ajax";
+	  $this->template->title =  "ToDo-list";
+	  $this->template->robots =  "index,follow";
+	
+    $session = $this->getSession('session');
+
+		if($this->getUser()->isLoggedIn()) { // přihlášení uživatelé
+			$this->template->loggedAs = "Přihlášen jako " . $this->getUser()->identity->data[0] . " (" . $this->getRole($this->getUser()) . ")";
+    }else{
+      $this->redirect('Homepage:');
+    }
+    	$this->makeMenu($this->getUser()->isLoggedIn());
+  }
+}
